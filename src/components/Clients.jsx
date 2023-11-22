404265dc5217134ce91f20e0cba7259d2d945c92
martindevaprasad <martindevaprasad46@gmail.com>
2023-11-22 14:25:55 +0530

    allissuefixede

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button, Card, CardFooter, Image } from "@nextui-org/react";
-import smartschool from "../assets/Veeta.webp";
-import kalai from "../assets/Kalai.webp";
-import Dbtr from "../assets/Dbtr.webp";
-import Aathi from "../assets/Aathi.webp";
-import veeta from "../assets/smartplayschoolcampuzone.webp";
+import smartschool from "../assets/Veeta.jpg";
+import kalai from "../assets/Kalai.jpg";
+import Dbtr from "../assets/Dbtr.jpg";
+import Aathi from "../assets/Aathi.png";
+import veeta from "../assets/smartplayschoolcampuzone.png";
 export const Clients = () => {
   const ClientArr = [smartschool, veeta, kalai, Dbtr, Aathi];
   return (
